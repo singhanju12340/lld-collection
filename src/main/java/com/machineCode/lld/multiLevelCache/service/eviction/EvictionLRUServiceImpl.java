6c9a4a56a5dc214d4a8acbe332f6e8ec3d811319
akumari <singhanju12340@gmail.com>
2025-05-19 23:45:11 +0530

    removeEldestEntry

--- src/main/java/com/machineCode/lld/multiLevelCache/service/eviction/EvictionLRUServiceImpl.java
+++ src/main/java/com/machineCode/lld/multiLevelCache/service/eviction/EvictionLRUServiceImpl.java
@@ -9,6 +9,16 @@ public class EvictionLRUServiceImpl<K> implements EvictionService<K>{
 
     LinkedHashMap<K, Boolean> linkedHashMap = new LinkedHashMap<>(10, 0.75f ,true); // enable access = true, maintains accecss order
 
+    // removeEldestEntry can directly remove eldest entry if size is greater than 10
+    // linkedHashMap is implementation of doubly linked list
+    LinkedHashMap<K, Boolean> linkedHashMap2 = new LinkedHashMap<>(10, 0.75f ,true
+    ){
+        @Override
+        protected boolean removeEldestEntry(java.util.Map.Entry<K, Boolean> eldest) {
+            return size() > 10;
+        }
+    };
+
     @Override
     public K getEvictionKey() {
         // get first key ion the linked hashmap
